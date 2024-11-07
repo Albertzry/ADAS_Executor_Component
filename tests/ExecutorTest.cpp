ca4fe7003a482cbb62c63aec696eded91b6fdb7b
Albertzry <albertzry@qq.com>
2024-11-07 22:37:52 +0800

    test:Pass move command cases and restore the initial two commands

--- tests/ExecutorTest.cpp
+++ tests/ExecutorTest.cpp
@@ -8,6 +8,24 @@ bool operator==(const Pose& lhs, const Pose& rhs)
 {  // 为Pose定义==运算符重载，使得在测试中比较Pose对象变得简单明了， std::tie提高代码可读性
     return std::tie(lhs.x, lhs.y, lhs.heading) == std::tie(rhs.x, rhs.y, rhs.heading);
 }
+TEST(ExecutorTest, should_return_init_pose_when_without_command)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+    // when		测试遵循了 Arrange-Act-Assert (AAA) 模式，这是提高可读性和维护性的好习惯
+    // then
+    const Pose target({0, 0, 'E'});
+    ASSERT_EQ(target, executor->Query());
+}
+TEST(ExecutorTest, should_return_default_pose_when_without_init_and_command)
+{
+    // given
+    std::unique_ptr<Executor> executor(Executor::NewExecutor());
+    // when
+    // then
+    const Pose target({0, 0, 'N'});
+    ASSERT_EQ(target, executor->Query());
+}
 
 // M
 TEST(ExecutorTest, should_return_x_plus_1_given_command_is_M_and_facing_is_E)
