a35a0eb099d6409d5c63faea9f010a96835fa21b
Albertzry <albertzry@qq.com>
2024-11-08 10:40:22 +0800

    test:add turn left and turn right commands and passed 14tests

--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -31,6 +31,44 @@ namespace adas
                     --pose.y;
                 }
             }
+            else if (cmd == 'L')
+            {
+                if (pose.heading == 'E')
+                {
+                    pose.heading = 'N';
+                }
+                else if (pose.heading == 'W')
+                {
+                    pose.heading = 'S';
+                }
+                else if (pose.heading == 'N')
+                {
+                    pose.heading = 'W';
+                }
+                else if (pose.heading == 'S')
+                {
+                    pose.heading = 'E';
+                }
+            }
+            else if (cmd == 'R')
+            {
+                if (pose.heading == 'E')
+                {
+                    pose.heading = 'S';
+                }
+                else if (pose.heading == 'W')
+                {
+                    pose.heading = 'N';
+                }
+                else if (pose.heading == 'N')
+                {
+                    pose.heading = 'E';
+                }
+                else if (pose.heading == 'S')
+                {
+                    pose.heading = 'W';
+                }
+            }
         }
     }
 
