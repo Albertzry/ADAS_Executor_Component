7dc711bf495e723eea21404c29251dc402e3c4b7
Albertzry <albertzry@qq.com>
2024-12-02 13:43:20 +0800

    Interface programming and use using to simplfy code

---
+++ src/CmderFactory.hpp
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <functional>
+#include <list>
+#include <unordered_map>
+#include "Command.hpp"
+
+namespace adas
+{
+using Cmder = std::function<void(PoseHandler& poseHandler)>;
+using CmderList = std::list<Cmder>;
+
+class CmderFactory final
+{
+// public:
+//     CmderFactory(void) noexcept = default;
+//     ~CmderFactory(void) noexcept = default;
+//     CmderFactory(const CmderFactory&) noexcept = delete;
+//     CmderFactory& operator=(const CmderFactory&) noexcept = delete;
+
+public:
+    // std::list<std::function<void(PoseHandler& poseHandler)>> GetCmders(const std::string& commands) const noexcept;
+    CmderList GetCmders(const std::string& commands) const noexcept;
+
+private:
+    const std::unordered_map<char, Cmder> cmderMap{
+        {'M', MoveCommand()},
+        {'L', TurnLeftCommand()},
+        {'R', TurnRightCommand()},
+        {'F', FastCommand()},
+        {'B', ReverseCommand()},
+    };
+};
+}  // namespace adas
--- src/ExecutorImpl.cpp
+++ src/ExecutorImpl.cpp
@@ -2,6 +2,9 @@
 #include <memory>
 #include "Command.hpp"
 #include <unordered_map>
+#include "CmderFactory.hpp"
+#include "Singleton.hpp"
+
 
 namespace adas
 {
@@ -15,114 +18,24 @@ namespace adas
 
     void ExecutorImpl::Execute(const std::string& commands) noexcept
     {
-    //     for (const auto cmd : commands)
-    //     {
-    //         std::unique_ptr<ICommand> cmder;
-    //         if (cmd == 'M')
-    //         {
-    //             cmder = std::make_unique<MoveCommand>();
-    //         }
-    //         else if (cmd == 'L')
-    //         {
-    //             cmder = std::make_unique<TurnLeftCommand>();
-    //         }
-    //         else if (cmd == 'R')
-    //         {
-    //             cmder = std::make_unique<TurnRightCommand>();
-    //         }
-    //         else if (cmd == 'F')
-    //         {
-    //             cmder = std::make_unique<FastCommand>();
-    //         }
-    //         else if (cmd == 'B') {
-    //             cmder = std::make_unique<ReverseCommand>();
-    //         }
-    //         if (cmder) {
-    //             cmder->DoOperate(*this);
-    //         }
-    //     }
+        // const std::unordered_map<char, std::function<void(PoseHandler & poseHandler)>> cmderMap {
+        //     {'M', MoveCommand()},
+        //     {'L', TurnLeftCommand()},
+        //     {'R', TurnRightCommand()},
+        //     {'F', FastCommand()},
+        //     {'B', ReverseCommand()},
+        // };
 
-        // std::unordered_map<char, std::function<void(PoseHandler & poseHandler)>> cmderMap;
-        // cmderMap.emplace('M', MoveCommand());
-        // cmderMap.emplace('L', TurnLeftCommand());
-        // cmderMap.emplace('R', TurnRightCommand());
-        // cmderMap.emplace('F', FastCommand());
-
-        const std::unordered_map<char, std::function<void(PoseHandler & poseHandler)>> cmderMap {
-            {'M', MoveCommand()},
-            {'L', TurnLeftCommand()},
-            {'R', TurnRightCommand()},
-            {'F', FastCommand()},
-            {'B', ReverseCommand()},
-        };
-
-        for (const auto cmd : commands) {
-            const auto it = cmderMap.find(cmd);
-            if (it != cmderMap.end()) {
-                it->second(poseHandler);
-            }
-        }
+        // for (const auto cmd : commands) {
+        //     const auto it = cmderMap.find(cmd);
+        //     if (it != cmderMap.end()) {
+        //         it->second(poseHandler);
+        //     }
+        // }
+        const auto cmders = Singleton<CmderFactory>::Instance().GetCmders(commands);
+        
+        std::for_each(cmders.begin(), cmders.end(), [this](const Cmder& cmder) noexcept { cmder(poseHandler); });
     }
-
-    // void ExecutorImpl::Move() noexcept
-    // {
-    //     if (pose.heading == 'E') {
-    //         ++pose.x;
-    //     } else if (pose.heading == 'W') {
-    //         --pose.x;
-    //     } else if (pose.heading == 'N') {
-    //         ++pose.y;
-    //     } else if (pose.heading == 'S') {
-    //         --pose.y;
-    //     }
-    // }
-    // void ExecutorImpl::TurnLeft() noexcept
-    // {
-    //     if (pose.heading == 'E')
-    //     {
-    //         pose.heading = 'N';
-    //     }
-    //     else if (pose.heading == 'W')
-    //     {
-    //         pose.heading = 'S';
-    //     }
-    //     else if (pose.heading == 'N')
-    //     {
-    //         pose.heading = 'W';
-    //     }
-    //     else if (pose.heading == 'S')
-    //     {
-    //         pose.heading = 'E';
-    //     }
-    // }
-    // void ExecutorImpl::TurnRight() noexcept
-    // {
-    //     if (pose.heading == 'E')
-    //     {
-    //         pose.heading = 'S';
-    //     }
-    //     else if (pose.heading == 'W')
-    //     {
-    //         pose.heading = 'N';
-    //     }
-    //     else if (pose.heading == 'N')
-    //     {
-    //         pose.heading = 'E';
-    //     }
-    //     else if (pose.heading == 'S')
-    //     {
-    //         pose.heading = 'W';
-    //     }
-    // }
-
-    // void ExecutorImpl::Fast() noexcept
-    // {
-    //     fast = !fast;
-    // }
-    // bool ExecutorImpl::IsFast() const noexcept
-    // {
-    // return fast;
-    // }
 
     Pose ExecutorImpl::Query() const noexcept
     {
